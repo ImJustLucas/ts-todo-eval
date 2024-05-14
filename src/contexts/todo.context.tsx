80fca21de43624ab78295c22f18d2bc480529321
ImJustLucas <lucas.bellier@icloud.com>
2024-05-14 15:46:07 +0200

    ♻️ Update TodoContext methods names for consistency

--- src/contexts/todo.context.tsx
+++ src/contexts/todo.context.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ToDoItem } from "../types";
 import fakeData from "./fakeData";
 
@@ -7,8 +7,8 @@ import fakeData from "./fakeData";
 interface ITodoContext {
   tasks: ToDoItem[];
   addTask: <T extends ToDoItem>(todo: T) => void;
-  removeTaks: <T extends ToDoItem>(todo: T) => void;
-  updateTaks: <T extends ToDoItem>(id: number, todo: T) => void;
+  removeTask: <T extends ToDoItem>(todo: T) => void;
+  updateTask: <T extends ToDoItem>(id: number, todo: T) => void;
 }
 
 const TodoContext = React.createContext<ITodoContext>({} as ITodoContext);
@@ -16,17 +16,17 @@ const TodoContext = React.createContext<ITodoContext>({} as ITodoContext);
 const TodoProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const [todos, setTodos] = React.useState<ToDoItem[]>(fakeData);
+  const [todos, setTodos] = useState<ToDoItem[]>(fakeData);
 
   const addTask = <T extends ToDoItem>(todo: T) => {
     setTodos([...todos, todo]);
   };
 
-  const removeTaks = <T extends ToDoItem>(todo: T) => {
+  const removeTask = <T extends ToDoItem>(todo: T) => {
     setTodos(todos.filter((t) => t.id !== todo.id));
   };
 
-  const updateTaks = <T extends ToDoItem>(id: number, todo: T) => {
+  const updateTask = <T extends ToDoItem>(id: number, todo: T) => {
     setTodos(todos.map((t) => (t.id === id ? todo : t)));
   };
 
@@ -35,8 +35,8 @@ const TodoProvider: React.FC<{
       value={{
         tasks: todos,
         addTask,
-        removeTaks,
-        updateTaks,
+        removeTask,
+        updateTask,
       }}
     >
       {children}
