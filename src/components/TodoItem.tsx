0e0dafb1cdac8cc430a1cf664b44a94baf8c0af4
YesIDo49 <mehdialsidcheikh@gmail.com>
2024-05-18 19:51:17 +0200

    :lipstick: update style of todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,53 +1,36 @@
 import { ToDoItem } from "../types";
+import {Checkbox} from "@material-tailwind/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 export const TodoItem: React.FC<{ todo: ToDoItem }> = ({ todo }) => {
-  //bg-gradient-to-r from-transparent to-transparent hover:from-slate-100 transition ease-linear duration-150
 
   return (
     <div
       id="task"
-      className={`flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4 border-l-transparent ${
-        todo.status === "Pending"
-          ? `bg-gradient-to-r from-transparent to-transparent hover:from-slate-100 transition ease-linear duration-150`
-          : todo.status === "InProgress"
-          ? `border-l-indigo-300 bg-gradient-to-r from-indigo-100 to-transparent hover:from-indigo-200 transition ease-linear duration-150`
-          : ""
+      className={`flex justify-between items-center transition-all ease-linear duration-150 border-b border-slate-200 py-1.5 px-2 border-l-4 border-l-transparent ${
+        todo.priority === "High"
+          ? `border-l-red-300 bg-gradient-to-r from-pink-50 to-transparent hover:from-pink-100`
+          : todo.priority === "Medium"
+          ? `border-l-indigo-200 bg-gradient-to-r from-blue-50 to-transparent hover:from-blue-100`
+          : "bg-gradient-to-r from-transparent to-transparent hover:from-gray-100"
       }`}
     >
       <div className="inline-flex items-center space-x-2">
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-slate-500 hover:text-indigo-600 hover:cursor-pointer"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+          <Checkbox color={todo.priority === "High" ? "pink" : todo.priority === "Medium" ? "blue" : ""} defaultChecked={todo.status === "InProgress"} />
         </div>
-        <div>{todo.description || "oueee"}</div>
+        <div className={`px-2 ${
+            todo.status === "InProgress"
+                ? `line-through text-gray-400`
+                    : ""
+        }`}>
+          {todo.description || "oueee"}
+        </div>
       </div>
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-4 h-4 text-slate-500 hover:text-slate-700 hover:cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-          />
-        </svg>
+        <FontAwesomeIcon className="text-gray-600 transition-all ease-in-out hover:text-red-500 cursor-pointer" icon={faTrashCan}/>
       </div>
     </div>
   );
